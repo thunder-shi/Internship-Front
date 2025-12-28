7e8a3d7acdcc7233c2f3584a9f3894cfd336550e
lzh <1185312007@qq.com>
2025-12-28 19:13:32 +0800

    岗位信息

---
+++ src/views/basic-information-manage/MainPost.vue
@@ -0,0 +1,66 @@
+<template>
+  <BaseList :default-props="defaultProps" :baselistConfirm="baselistConfirm" ref="baseList" />
+</template>
+<script setup>
+import { reactive, ref } from 'vue';
+import BaseList from '@/views/master-page/BaseList.vue';
+
+defineOptions({
+  name: 'MainPost',
+});
+
+const baseList = ref(null);
+const defaultProps = reactive({
+  defaultDTLProps: {
+    title: { mainTitle: '岗位信息管理' },
+    defaultDTHProps: {
+      buttonProps: {
+        update: { show: true },
+        create: { show: true },
+        delete: { show: true },
+        export: { show: true },
+        up: { show: true },
+        down: { show: true },
+        batchCreate: { show: true },
+        audit: { show: true },
+      },
+      keyWord: { edit: 'MainPost', view: 'ViewMainPost' },
+      allTableColumns: [
+        { id: 1, showName: '企业', theOrder: 1, tableColumnName: 'departmentName', sortable: true },
+        { id: 1, showName: '岗位名称', theOrder: 1, tableColumnName: 'name', sortable: true },
+        { id: 1, showName: '岗位代码', theOrder: 1, tableColumnName: 'code', sortable: true },
+        { id: 2, showName: '岗位人数', theOrder: 2, tableColumnName: 'personNum', sortable: true },
+        { id: 3, showName: '薪资', theOrder: 3, tableColumnName: 'salary' },
+      ],
+    },
+    defaultDBIProps: {},
+  },
+  defaultSDProps: {
+    keyWord: 'MainPost',
+    formItems: [
+      { name: '企业', field: 'companyId', type: 'select', keyWords: 'BaseDepartment' },
+      { name: '岗位名称', field: 'name', type: 'input' },
+      { name: '岗位代码', field: 'code', type: 'input' },
+      { name: '岗位人数', field: 'personNum', type: 'input' },
+      { name: '薪资', field: 'salary', type: 'input' },
+    ],
+    formRules: {
+      companyId: [{ required: true, message: '企业不能为空', trigger: 'blur' }],
+      name: [{ required: true, message: '岗位名称不能为空', trigger: 'blur' }],
+      code: [{ required: true, message: '岗位代码不能为空', trigger: 'blur' }],
+      personNum: [{ required: true, message: '岗位人数不能为空', trigger: 'blur' }],
+      salary: [{ required: true, message: '薪资不能为空', trigger: 'blur' }],
+    },
+    defaultDBProps: {
+      dialog: {},
+    },
+  },
+  defaultDBIProps: {
+    keyWords: 'MainPost',
+  },
+});
+
+const baselistConfirm = (option, type) => {
+  baseList.value._confirm(option, type, { isAudit: -1 });
+};
+</script>
--- src/views/master-page/BaseList.vue
+++ src/views/master-page/BaseList.vue
@@ -1,63 +1,83 @@
 <template>
   <div>
-    <DataTableList ref="dataTableList" :default-props="defaultProps.defaultDTLProps" @append-click="appendClick" @edit-click="editClick" @update-column="updateColumn" @delete-click="deleteClick" @export-click="exportClick" @more1-click="more1Click" @more2-click="more2Click" @after-init-data="afterInitData">
+    <DataTableList
+      ref="dataTableList"
+      :default-props="defaultProps.defaultDTLProps"
+      @append-click="appendClick"
+      @edit-click="editClick"
+      @update-column="updateColumn"
+      @delete-click="deleteClick"
+      @export-click="exportClick"
+      @more1-click="more1Click"
+      @more2-click="more2Click"
+      @after-init-data="afterInitData"
+    >
       <!--数据操作按钮类 -->
-    <!-- <template #searchPanel>
+      <!-- <template #searchPanel>
       <slot name="searchPanel" />
     </template> -->
     </DataTableList>
     <slot name="dlg">
       <!-- 简单窗口 -->
-      <SimpleDialog ref="simpleDialog" :default-props="defaultProps.defaultSDProps" :simpledialog-confirm="confirm" @update-record="initDataList" @submit-more="submitMore" @simple-select-change="SimpleSelectChange" />
+      <SimpleDialog
+        ref="simpleDialog"
+        :default-props="defaultProps.defaultSDProps"
+        :simpledialog-confirm="confirm"
+        @update-record="initDataList"
+        @submit-more="submitMore"
+        @simple-select-change="SimpleSelectChange"
+      />
     </slot>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive, onMounted, useAttrs } from 'vue'
-import DataTableList from '@/components/DataTableList.vue'
-import SimpleDialog from '@/components/SimpleDialog.vue'
-import { resetForm } from '@/utils/common'
+import { ref, reactive, onMounted, useAttrs } from 'vue';
+import DataTableList from '@/components/DataTableList.vue';
+import SimpleDialog from '@/components/SimpleDialog.vue';
+import { resetForm } from '@/utils/common';
 
 defineOptions({
-  name: 'BaseList'
-})
+  name: 'BaseList',
+});
 
 const props = defineProps({
   defaultProps: {
     type: Object,
     default: () => {
       return {
-        keyWord: { },
+        keyWord: {},
         defaultDTLProps: {
           title: {},
-          initSearchWords: { // 初始时查询的三个关键词
-            searchKey: { },
-            regKey: { },
-            andor: { }
+          initSearchWords: {
+            // 初始时查询的三个关键词
+            searchKey: {},
+            regKey: {},
+            andor: {},
           },
-          nowSearchWords: { // 变化查询的三个关键词
-            searchKey: { },
-            regKey: { },
-            andor: { }
+          nowSearchWords: {
+            // 变化查询的三个关键词
+            searchKey: {},
+            regKey: {},
+            andor: {},
           },
           defaultDTHProps: {
-            allTableColumns: { },
-            buttonProps: { }
-          }
+            allTableColumns: {},
+            buttonProps: {},
+          },
         },
         defaultSDProps: {
           // defaultDBProps: {
           //   footButtons: { }
           // }
-        }
-      }
-    }
+        },
+      };
+    },
   },
   baselistSpecConfirm: { type: Function, default: null },
   baselistConfirm: { type: Function, default: null },
-  searchPlaceholder: { type: String, default: '请输入名称' }
-})
+  searchPlaceholder: { type: String, default: '请输入名称' },
+});
 
 const emit = defineEmits([
   'update-column',
@@ -69,37 +89,36 @@ const emit = defineEmits([
   'more1-click',
   'more2-click',
   'submit-more',
-  'simple-select-change'
-])
+  'simple-select-change',
+]);
 
-const attrs = useAttrs()
+const attrs = useAttrs();
 
-const dataTableList = ref(null)
-const simpleDialog = ref(null)
+const dataTableList = ref(null);
+const simpleDialog = ref(null);
 
-const form = reactive({}) // 关联form
-const keyWord = reactive({ edit: '', view: '' })
-const treeInfo = reactive({})
-const searchName = ref('')
-const thisEvents = ref('')
-
+const form = reactive({}); // 关联form
+const keyWord = reactive({ edit: '', view: '' });
+const treeInfo = reactive({});
+const searchName = ref('');
+const thisEvents = ref('');
 onMounted(() => {
   // 获取所有事件监听器
-  thisEvents.value = Object.keys(attrs).filter(key => key.startsWith('on'))
-})
+  thisEvents.value = Object.keys(attrs).filter((key) => key.startsWith('on'));
+});
 
 const initDataList = async () => {
-  dataTableList.value?.initDataList()
-}
+  dataTableList.value?.initDataList();
+};
 
 // 当前行的改变
 const updateColumn = (val) => {
-  emit('update-column', val)
-}
+  emit('update-column', val);
+};
 
 const afterInitData = (dataList) => {
-  emit('after-init-data', dataList)
-}
+  emit('after-init-data', dataList);
+};
 
 // #region  DataList对应按钮事件
 const searchClick = () => {
@@ -107,75 +126,75 @@ const searchClick = () => {
   // this.searchKey = { name: this.searchName }
   // // 父子组件传递数据不及时的问题
   // setTimeout(() => { initDataList() }, 500)
-}
+};
 
 const appendClick = async () => {
   if (attrs['onAppend-click'] || attrs['onAppendClick']) {
-    emit('append-click')
+    emit('append-click');
   } else {
-    Object.assign(form, resetForm(form))
-    simpleDialog.value?.showDialog(true, form)
+    Object.assign(form, resetForm(form));
+    simpleDialog.value?.showDialog(true, form);
   }
-}
+};
 
 const editClick = async (row) => {
   if (!attrs['onEdit-click'] && !attrs['onEditClick']) {
-    openDlg('edit', row)
+    openDlg('edit', row);
   } else {
-    emit('edit-click', row)
+    emit('edit-click', row);
   }
-}
+};
 
 const deleteClick = async (row) => {
   if (attrs['onDelete-click'] || attrs['onDeleteClick']) {
-    emit('delete-click', row)
+    emit('delete-click', row);
   } else {
-    dataTableList.value?._deleteClick(row)
+    dataTableList.value?._deleteClick(row);
   }
-}
+};
 
 // 导出按钮
 const _exportClick = async () => {
-  await dataTableList.value?._exportClick()
-}
+  await dataTableList.value?._exportClick();
+};
 
 const exportClick = async () => {
   if (attrs['onExport-click'] || attrs['onExportClick']) {
-    emit('export-click')
+    emit('export-click');
   } else {
-    await _exportClick()
+    await _exportClick();
   }
-}
+};
 
 // 更多内容1
 const more1Click = async (row) => {
-  emit('more1-click', row)
-}
+  emit('more1-click', row);
+};
 
 // 更多内容2
 const more2Click = async (row) => {
-  emit('more2-click', row)
-}
+  emit('more2-click', row);
+};
 // #endregion
 
 // #region simpleDialog对应的按钮事件
 const confirm = async (option, type) => {
   if (!(props.baselistConfirm && typeof props.baselistConfirm === 'function')) {
-    await simpleDialog.value?._confirm(option, type)
+    await simpleDialog.value?._confirm(option, type);
   } else {
-    await props.baselistConfirm(option, type, form)
+    await props.baselistConfirm(option, type, form);
   }
-}
+};
 
 const submitMore = (row) => {
-  emit('submit-more', row)
-}
+  emit('submit-more', row);
+};
 
 const openDlg = (option, row) => {
   if (row != null) {
-    Object.assign(form, { ...row })
+    Object.assign(form, { ...row });
   } else {
-    Object.assign(form, resetForm(form))
+    Object.assign(form, resetForm(form));
   }
   // if (option == null) {
   //   option = this.dialog.option
@@ -186,17 +205,20 @@ const openDlg = (option, row) => {
   } else if (option === 'append') {
     // Object.assign(this.dialog, { title: '新增', option: 'append' })
   }
-  simpleDialog.value?.showDialog(true, form)
-}
+  simpleDialog.value?.showDialog(true, form);
+};
 
 const SimpleSelectChange = (val, field, form, options) => {
-  emit('simple-select-change', val, field, form, options)
-}
+  emit('simple-select-change', val, field, form, options);
+};
 // #endregion
 
 // 暴露方法给父组件
 defineExpose({
   initDataList,
-  openDlg
-})
+  openDlg,
+  _confirm: (option, type, form) => {
+    simpleDialog.value?._confirm(option, type, form);
+  },
+});
 </script>
