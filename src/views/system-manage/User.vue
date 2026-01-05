b039234ea2b000f6520a65b87e9f80794477b507
lzh <1185312007@qq.com>
2026-01-05 13:27:05 +0800

    [用户管理]添加批量导入

--- src/views/system-manage/User.vue
+++ src/views/system-manage/User.vue
@@ -1,66 +1,81 @@
 <template>
-<BaseTreeList ref="baseTreeList" :default-props="defaultProps" @append-click="appendClick" @edit-click="editClick">
-    <template #dlg>
-      <DlgUserInfo ref="dlgUser" @update-record="initDataList" />
-    </template>
+  <BaseTreeList
+    ref="baseTreeList"
+    :default-props="defaultProps"
+    @append-click="appendClick"
+    @edit-click="editClick"
+  >
+    <template #dlg> <DlgUserInfo ref="dlgUser" @update-record="initDataList" /> </template>
   </BaseTreeList>
 </template>
 <script setup>
-import { reactive, ref } from 'vue'
-import BaseTreeList from '@/views/master-page/BaseTreeList.vue'
-import DlgUserInfo from '@/views/dialogs/DlgUserInfo.vue'
+import { reactive, ref } from 'vue';
+import BaseTreeList from '@/views/master-page/BaseTreeList.vue';
+import DlgUserInfo from '@/views/dialogs/DlgUserInfo.vue';
 
 defineOptions({
-  name: 'User'
-})
+  name: 'User',
+});
 
-const baseTreeList = ref(null)
-const dlgUser = ref(null)
+const baseTreeList = ref(null);
+const dlgUser = ref(null);
 
 const defaultProps = reactive({
   treeRelColName: 'departmentId',
   defaultDTLProps: {
     title: { mainTitle: '人员列表' },
     someFlags: {
-      noAdvancedSearch: false
+      noAdvancedSearch: false,
     },
     searchItems: [
       { name: '姓名', field: 'name', type: 'input' },
-      { name: '手机号', field: 'phone', type: 'input' }
+      { name: '手机号', field: 'phone', type: 'input' },
     ],
     defaultDTHProps: {
       keyWord: { edit: 'BaseUser', view: 'ViewBaseUser' },
-      allTableColumns: { },
-      buttonProps: { update: { show: true }, create: { show: false }, delete: { show: true }, export: { show: false }, search: { show: true }, batchCreate: { show: false }}
-    }
+      allTableColumns: {},
+      buttonProps: {
+        update: { show: true },
+        create: { show: false },
+        delete: { show: true },
+        export: { show: false },
+        search: { show: true },
+        batchCreate: { show: true },
+      },
+    },
   },
   defaultDTProps: {
     keyWord: 'BaseDepartment',
-    title: { mainTitle: '单位部门列表' }
-  }
-})
+    title: { mainTitle: '单位部门列表' },
+  },
+  defaultDBIProps: {
+    keyWords: 'BaseUser',
+  },
+});
 
-const allTableColumns = defaultProps.defaultDTLProps.defaultDTHProps.allTableColumns
+const allTableColumns = defaultProps.defaultDTLProps.defaultDTHProps.allTableColumns;
 Object.assign(allTableColumns, [
   { id: 1, showName: '单位', theOrder: 1, tableColumnName: 'departmentName' },
   { id: 2, showName: '姓名', theOrder: 2, tableColumnName: 'name', sortable: true },
   { id: 3, showName: '职务', theOrder: 3, tableColumnName: 'jobName' },
-  { id: 4, showName: '手机号', theOrder: 4, tableColumnName: 'phone', sortable: true }
-])
+  { id: 4, showName: '手机号', theOrder: 4, tableColumnName: 'phone', sortable: true },
+]);
 
-const form = reactive({ id: null, workId: null, name: '' })
+const form = reactive({ id: null, workId: null, name: '' });
 
 const initDataList = async () => {
-  await baseTreeList.value?.initDataList()
-}
+  console.log(1);
+
+  await baseTreeList.value?.initDataList();
+};
 
 const appendClick = () => {
-  Object.assign(form, { id: null, workId: null, name: '' })
-  dlgUser.value?.showDialog(true, form)
-}
+  Object.assign(form, { id: null, workId: null, name: '' });
+  dlgUser.value?.showDialog(true, form);
+};
 
 const editClick = (val) => {
-  Object.assign(form, val)
-  dlgUser.value?.showDialog(true, form)
-}
+  Object.assign(form, val);
+  dlgUser.value?.showDialog(true, form);
+};
 </script>
