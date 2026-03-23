ea41b2f77da515aa419e548f539bdc0912400003
lzh <1185312007@qq.com>
2026-03-23 17:08:26 +0800

    分配校内导师审核[待调]

---
+++ src/views/internship-process/InternalTutorAssignmentVerify.vue
@@ -0,0 +1,22 @@
+<template>
+  <AssignmentVerifyPage
+    page-title="分配校内导师审核"
+    no-project-message="当前没有需要审核的校内导师分配数据"
+    :process-type-code="processTypeCode"
+    dlg-title="分配校内导师审核"
+    recall-title="退回已通过的校内导师分配"
+    :table-columns="VERIFY_TEACHER_COLUMNS"
+  />
+</template>
+
+<script setup>
+import CONSTANT from '@/utils/constant';
+import AssignmentVerifyPage from './components/AssignmentVerifyPage.vue';
+import { VERIFY_TEACHER_COLUMNS } from './config/assignmentPresets';
+
+defineOptions({
+  name: 'InternalTutorAssignmentVerify',
+});
+
+const processTypeCode = CONSTANT.PROCESS_TYPE.EXTERNAL_ASSIGN_INTERNAL_TUTOR;
+</script>
